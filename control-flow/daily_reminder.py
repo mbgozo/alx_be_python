385f2a9cc385d0e73bdc9e78344264e5bc1a185b
Michael B Gozo <mbgozobuilds@gmail.com>
2025-11-18 13:12:28 +0000

    corrected the right match pattern

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,20 +2,20 @@ task_var = input("Enter your task: ")
 task_priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
-match task_priority:
-    case n if n == 'high':
+match task_priority.lower():
+    case 'high':
         if time_bound.lower() == "yes":
             print(f"Reminder:'{task_var}' is a high priority task that requires immediate attention today!")
         else:
             print(f"Reminder: '{task_var}' is a high priority task. Aim to schedule time for it today.")
     
-    case n if n == 'medium':
+    case 'medium':
         if time_bound.lower() == 'yes':
             print(f"Reminder: '{task_var}' is a medium priority task with a deadline. Plan accordingly.")
         else:
             print(f"Note: '{task_var}' is a medium priority task. Schedule time for it this week.")
 
-    case n if n == 'low':
+    case 'low':
         if time_bound.lower() == 'no':
             print(f"'{task_var}' is a low priority task. Consider completing it when you have free time")
         else:
