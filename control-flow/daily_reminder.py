a05dd28bc1a2e9742c20350c2426f2005ac99557
Michael B Gozo <mbgozobuilds@gmail.com>
2025-11-18 13:19:00 +0000

    convert 'time_bound' to lower case

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,22 @@
 task_var = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ").lower()
-time_bound = input("Is it time-bound? (yes/no): ")
+time_bound = input("Is it time-bound? (yes/no): ").lower()
 
 match priority:
     case 'high':
-        if time_bound.lower() == "yes":
+        if time_bound == "yes":
             print(f"Reminder:'{task_var}' is a high priority task that requires immediate attention today!")
         else:
             print(f"Reminder: '{task_var}' is a high priority task. Aim to schedule time for it today.")
     
     case 'medium':
-        if time_bound.lower() == 'yes':
+        if time_bound == 'yes':
             print(f"Reminder: '{task_var}' is a medium priority task with a deadline. Plan accordingly.")
         else:
             print(f"Note: '{task_var}' is a medium priority task. Schedule time for it this week.")
 
     case 'low':
-        if time_bound.lower() == 'no':
+        if time_bound == 'no':
             print(f"'{task_var}' is a low priority task. Consider completing it when you have free time")
         else:
             print(f"Note: '{task_var}' is low priority but time-bound. Ensure it is completed before the deadline.")
