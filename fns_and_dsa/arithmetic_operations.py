447a899823a3120d99bcf2ef9f4dcc4d9514007f
Michael B Gozo <mbgozobuilds@gmail.com>
2025-11-23 16:26:16 +0000

    Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1, num2, string):
+    if string in ["add", "subtract", 'multiply', "divide"]:
+        if string == 'divide' and num2 == 0:
+            return "Can't perfom division by Zero"
+        else:
+            if string == "add":
+                return num1 + num2
+            elif string == "subtract":
+                return num1 - num2
+            elif string == "multiply":
+                return num1 * num2
+            elif string == 'divide':
+                return num1 / num2
+    else:
+        return "Operation cant be handled atm, please try again later"
